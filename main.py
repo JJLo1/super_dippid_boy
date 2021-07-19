2d78ad29834bc01d0649f50c660aad882ff421b6
Akri <johannes.lorper1@gmail.com>
2021-07-20 01:16:40 +0200

    added more pyqt gui elements and handling

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ from obstacle import Obstacle, SharedObstacleState
 from gesture_recognizer.dollar_one_recognizer import DollarOneRecognizer
 from player_character import *
 import pygame
-# import pygame_menu
-# from ui_helper import UiHelper
 from PyQt5 import QtWidgets, uic, QtGui
+
 # pygame.locals puts a set of useful constants and functions into the global namespace of this script
 from pygame.locals import (
     MOUSEBUTTONUP,
@@ -29,53 +28,68 @@ if not pygame.font:
 if not pygame.mixer:
     raise SystemExit("[Error]: Pygame Sound disabled")
 
-
+vec = pygame.math.Vector2
 random.seed(42)  # set a random seed to make the game deterministic while testing
 
 
-# noinspection PyAttributeOutsideInit
 class SuperDippidBoy(QtWidgets.QWidget):
 
-    def __init__(self, dippid_port=5700):
+    def __init__(self):
         super().__init__()
         self.start_screen = QtWidgets.QWidget()
-        # self.__ui = uic.loadUi("assets/start_menu.ui")
-        self.dippid_sensor = SensorUDP(dippid_port)
+        self.__ui = uic.loadUi("assets/start_menu.ui", self)
+        self.setFixedSize(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.dippid_sensor = SensorUDP(5700)
 
         # init gesture recognizer
         self.gesture_recognizer = DollarOneRecognizer()
 
-        self.setup_game()
+        self.screen, self.background, self.background_rect = self.setup_game()
         self.background_width, self.background_height = self.background.get_size()
-        self.background_area = (0, 10, self.background_width, self.background_height)  # cut off 10 pixels at the top of the background
-        # self.ui_helper = UiHelper()
-        # self.show()
+        self.background_area = (
+        0, 10, self.background_width, self.background_height)  # cut off 10 pixels at the top of the background
+        self.show()
 
         self.sound_handler = SoundHandler()
         self.image_handler = ImageHandler()
         # TODO make sure the transition at the end when replaying is smooth!
         self.sound_handler.play_sound("mysterious_harp.mp3", play_infinite=True)  # start playing background music
-        self.run_game()
-        # self.run_start_screen()
+        # self.run_game()
+        self.run_start_screen()
 
     def end_game(self):
+
         # stop music
         pygame.mixer.music.stop()
         pygame.mixer.quit()
-
-        pygame.quit()
+        # stop dippid sensor
         self.dippid_sensor.disconnect()
-        sys.exit(0)
+        # quit pygame
+        pygame.quit()
+        # quit pyQt
+        self.close()
+        # TODO finish dippid device as well
 
     def setup_game(self):
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))   # setup the game window
+        # pygame needs a display to work properly, but since we use PyQt to display our game we will set it hidden
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.HIDDEN)
         pygame.display.set_caption(GAME_TITLE)
+        pygame.display.iconify()
 
         # setup background
-        background_image, self.background_rect = ImageHandler.load_background_image()
+        background_image, background_rect = ImageHandler.load_background_image()
         # scale the background image to fill the entire background
-        w, h = self.screen.get_size()
-        self.background = pygame.transform.smoothscale(background_image, [int(w), int(h)])
+        w, h = screen.get_size()
+        background = pygame.transform.smoothscale(background_image, [int(w), int(h)])
+
+        """
+        top_border_block = pygame.Surface((SCREEN_WIDTH, 50), pygame.SRCALPHA)  # pygame.SRCALPHA makes it transparent
+        top_border_rect = top_border_block.get_rect(topleft=(0, 0))
+
+        bottom_border_block = pygame.Surface((SCREEN_WIDTH, 50), pygame.SRCALPHA)
+        bottom_border_rect = bottom_border_block.get_rect(bottomleft=(0, SCREEN_HEIGHT))
+        """
+        return screen, background, background_rect  # , top_border_rect, bottom_border_rect
 
     def draw_background(self):
         # Display the background
@@ -83,25 +97,17 @@ class SuperDippidBoy(QtWidgets.QWidget):
         # Changes to the display surface are not immediately visible. Normally, a display must be updated in areas that
         # have changed for them to be visible to the user. With double buffered displays the display must be swapped
         # (or flipped) for the changes to become visible
-        pygame.display.flip()
+        # pygame.display.flip()
 
     def run_start_screen(self):
-        show_start_screen = True
-        clock = pygame.time.Clock()
         self.draw_background()
-        while show_start_screen:
-            clock.tick(FPS)
-
-            # pygame.event.pump()  # TODO use this instead?
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    show_start_screen = False
-
-            # Flip the contents of pygame's software double buffer to the screen.
-            pygame.display.flip()
+        self.menu_widget = self.__ui.GameMenu
+        self.menu_widget.move(self.rect().center() - self.menu_widget.rect().center())
+        self.__ui.StartGameButton.clicked.connect(self.run_game)
 
     def run_game(self):
-        main_character = PlayerCharacter(self.image_handler, self.sound_handler, graphics_folder="assets/triangle")
+        self.menu_widget.hide()
+        main_character = PlayerCharacter(self.image_handler, self.sound_handler, graphics_folder="assets/Triangle")
         obstacles = pygame.sprite.Group()  # for rendering all obstacles
         wall_collidables = pygame.sprite.Group()  # for collision detection
         gate_collidables = pygame.sprite.Group()
@@ -119,13 +125,11 @@ class SuperDippidBoy(QtWidgets.QWidget):
         UPDATE_SCORE_EVENT = pygame.USEREVENT + 3
         pygame.time.set_timer(UPDATE_SCORE_EVENT, 1000)  # update score each second
 
-        # HIDE_GESTURE_EVENT = pygame.USEREVENT + 4
-
-        # Clock object used to help control the game's framerate. Used in the main loop to make sure the game doesn't
-        # run too fast
+        # Clock object used to help control the game's framerate. Used in the main loop to make sure the game doesn't run
+        # too fast
         clock = pygame.time.Clock()
 
-        font = pygame.font.Font(None, 25)
+        font = pygame.font.Font(None, 25)  # TODO show the current point score on the screen
         current_points = 0
 
         self.draw_background()
@@ -136,13 +140,13 @@ class SuperDippidBoy(QtWidgets.QWidget):
 
         background_movement_speed = 1.5
         running = True
-
         # main game loop
         while running:
             # make sure the game doesn't run faster than the defined frames per second
             clock.tick(FPS)
-            # current_fps = clock.get_fps()
-            # print("Current fps: ", current_fps)
+            current_fps = clock.get_fps()
+            text = font.render(f"Current FPS: {current_fps}", True, (255, 0, 0))  # TODO only for testing
+            self.screen.blit(text, (10, 10))
 
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -190,17 +194,12 @@ class SuperDippidBoy(QtWidgets.QWidget):
 
                 elif event.type == UPDATE_SCORE_EVENT:
                     current_points += 5
-                """
-                elif event.type == HIDE_GESTURE_EVENT:
-                    show_gesture = False
-                    pygame.time.set_timer(HIDE_GESTURE_EVENT, 0)  # stop timer
-                """
-
             if "gravity" in self.dippid_sensor.get_capabilities():
                 main_character.change_movement(angle=self.dippid_sensor.get_value('gravity')['x'])
             # TODO: check if "angle" is bugged on m5stack, since the values seemed strange
             elif "angle" in self.dippid_sensor.get_capabilities():
                 main_character.change_movement(angle=self.dippid_sensor.get_value('angle')['x'])
+
             """
             # alternative:
             if dippid.get_value('gravity')['y'] > 1:
@@ -213,17 +212,19 @@ class SuperDippidBoy(QtWidgets.QWidget):
 
             keys = pygame.key.get_pressed()  # checking pressed keys
             if keys[pygame.K_w]:
-                main_character.change_movement(angle=-10)
-            elif keys[pygame.K_s]:
                 main_character.change_movement(angle=10)
+            elif keys[pygame.K_s]:
+                main_character.change_movement(angle=-10)
 
-            # draw background (erases everything from previous frame; quite inefficient but works for now)
+            # draw background (erases everything from previous frame (quite inefficient!))
             self.screen.blit(self.background, self.background_rect, area=self.background_area)
+            # more efficient:
+            # [screen.blit(background, sprite.rect, sprite.rect) for game_object_group in game_objects for sprite
+            #  in game_object_group.sprites()]
 
-            # Implement a scrolling background,
-            # see https://stackoverflow.com/questions/51320007/side-scrolling-background
-            # The new position of the upcoming background is calculated by simply moving the rect by the width of the
-            # image upcoming image is the same as the current one, just offset to the right
+            # Implement a scrolling background, see https://stackoverflow.com/questions/51320007/side-scrolling-background
+            # the new position of the upcoming background is calculated by simply moving the rect by the width of the image
+            # upcoming image is the same as the current one, just offset to the right
             upcoming_background = self.background_rect.move(self.background_rect.width, 0)
             self.screen.blit(self.background, upcoming_background, area=self.background_area)
 
@@ -248,9 +249,7 @@ class SuperDippidBoy(QtWidgets.QWidget):
 
             main_character.update()
             self.screen.blit(main_character.image, main_character.rect)
-            # TODO debug: show hitbox of player character
-            hitbox = (main_character.rect.x, main_character.rect.y, main_character.rect.width, main_character.rect.height)
-            pygame.draw.rect(self.screen, (255, 0, 0), hitbox, 2)
+            # pygame.draw.rect(self.screen,(255,0,0),main_character.rect)
 
             text_surface = font.render(f"Score: {current_points}", True, (255, 0, 0))
             self.screen.blit(text_surface, (SCREEN_WIDTH // 2 - 50, 15))
@@ -260,8 +259,8 @@ class SuperDippidBoy(QtWidgets.QWidget):
                 # If so, then remove the player and stop the loop
                 print("Player collided with wall! Game over!")
                 main_character.kill()
-                running = False
-                # TODO show 'You Died' - Message :)
+            # running = False
+            # TODO show 'You Died' - Message :)
 
             # if pygame.sprite.spritecollideany(main_character, gate_collidables, check_gate_collision):
 
@@ -279,9 +278,8 @@ class SuperDippidBoy(QtWidgets.QWidget):
 
             # Flip the contents of pygame's software double buffer to the screen.
             # This makes everything we've drawn visible all at once.
-            pygame.display.flip()
-
-            # update the QWidget as well
+            # pygame.display.flip()
+            # pygame.display.update(...)  # can be used instead to update only a part of the display
             self.update()
 
         # quit the game and clean up after the main loop finished
@@ -295,7 +293,12 @@ class SuperDippidBoy(QtWidgets.QWidget):
         qp.drawImage(0, 0, image)
         qp.end()
 
+    # Overiding close event, so we can close the pygame window and sounds too
+    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+        self.end_game()
 
+
+# TODO this main method is far too long -> extract most of it to a main class, e.g. "Game", as in the Praxisseminar
 def main():
     app = QtWidgets.QApplication(sys.argv)
     pygame.init()  # setup and initialize pygame
@@ -304,5 +307,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # TODO add argument parser with options port and debug_mode
     main()
