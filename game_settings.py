829dbc21ba0d516dba0be965053e1ddb3daaaf3b
michaelmeckl <michael.meckl@stud.uni-regensburg.de>
2021-07-21 20:34:27 +0200

    Make game width larger

--- game_settings.py
+++ game_settings.py
@@ -3,5 +3,5 @@ global game constants
 """
 
 GAME_TITLE = "SUPER DIPPID BOY"
-SCREEN_WIDTH, SCREEN_HEIGHT = 960, 640
+SCREEN_WIDTH, SCREEN_HEIGHT = 1200, 640
 FPS = 60  # the fps our game should run at
